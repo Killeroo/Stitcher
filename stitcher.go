a2d5e2deb43a07c5fc5fb496eb8dd766955d1d49
matthew carney <matthewcarney64@gmail.com>
2019-06-15 20:05:23 +0100

    Cleaned up main function Some small formatting changes

--- stitcher.go
+++ stitcher.go
@@ -2,25 +2,24 @@
 // Alot of credit to gombine (https://github.com/r3s/gombine) for providing inspiration to some
 // of the logic for obtaining image data (nioce code man)
 // Basic image manipulation stuff (https://stackoverflow.com/a/35965499)
- 
+
 package main
 
 import (
-	"image"
-	"image/png"
-	"image/draw"
-	"io/ioutil"
-	"strings"
-	"os"
 	"flag"
-	"net/http"
-	"log"
-	"path"
 	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strings"
 )
 
-const iconText =
-`
+const iconText = `
        (o\---/o)
         ( . . )
        _( (T) )_
@@ -33,11 +32,14 @@ const iconText =
 `
 
 type ImageData struct {
-	img	   image.Image
+	img    image.Image
 	height int
 	width  int
 	path   string
 }
+
+var fileCount int
+var images []*ImageData
 
 // Creates a new png using an array of image data. First function checks that all images are the same
 // size, then works out the dimensions of the new image, after which a new image is created and each image
@@ -56,27 +58,27 @@ func saveNewImage(images []*ImageData, cols int, outFile string) error {
 	// Work out new image dimensions
 	var newImg ImageData
 	var gridHeight int
-	if len(images) % cols == 0 {
+	if len(images)%cols == 0 {
 		newImg.height = maxHeight * len(images) / cols
 		gridHeight = len(images) / cols
 	} else {
-		newImg.height = maxHeight * ((len(images) / cols)+1)
-		gridHeight = (len(images) / cols)+1
+		newImg.height = maxHeight * ((len(images) / cols) + 1)
+		gridHeight = (len(images) / cols) + 1
 	}
 	newImg.width = maxWidth * cols
 	log.Printf("New image dimensions (%dx%d grid): x=%d y=%d", cols, gridHeight, newImg.width, newImg.height)
 
 	// Create new image
-	r := image.Rectangle{image.Point{0,0}, image.Point{newImg.width, newImg.height}}
+	r := image.Rectangle{image.Point{0, 0}, image.Point{newImg.width, newImg.height}}
 	rgba := image.NewRGBA(r)
 
 	// Copy over image data from stored images
 	var curHeight int
 	var curWidth int
 	for index, i := range images {
-		draw.Draw(rgba, image.Rectangle{image.Point{curWidth,curHeight}, image.Point{curWidth + i.width,curHeight + i.height}}, i.img, image.Point{0,0}, draw.Src)
+		draw.Draw(rgba, image.Rectangle{image.Point{curWidth, curHeight}, image.Point{curWidth + i.width, curHeight + i.height}}, i.img, image.Point{0, 0}, draw.Src)
 		log.Printf("Adding image [%d] @ %dx%d", index+1, curWidth, curHeight)
-		if (index+1) % cols == 0 {
+		if (index+1)%cols == 0 {
 			curHeight += i.height
 			curWidth = 0
 		} else {
@@ -86,7 +88,7 @@ func saveNewImage(images []*ImageData, cols int, outFile string) error {
 
 	// Save new image
 	log.Println("Saving new file...")
-	out, err := os.Create("./" + outFile  + ".png")
+	out, err := os.Create("./" + outFile + ".png")
 	defer out.Close()
 	if err != nil {
 		return err
@@ -124,7 +126,7 @@ func getImageData(img *image.Image, filename string) (ImageData, error) {
 
 // Checks if the file is a PNG.  First function opens the file, then 'sniffs' the first 512 bytes,
 // these bytes are then given to the detectcontenttype function (hacky i know) which ascertains
-// the file type, if it is a PNG then true is returned, else false is returned 
+// the file type, if it is a PNG then true is returned, else false is returned
 func isPNG(path string) (bool, error) {
 
 	file, err := os.Open(path)
@@ -138,16 +140,65 @@ func isPNG(path string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	
+
 	// Reset read pointer
 	file.Seek(0, 0)
-	
+
 	fileType := http.DetectContentType(buffer)
 	if strings.Compare(fileType, "image/png") == 0 {
 		return true, nil
 	} else {
 		return false, nil
 	}
+}
+
+func handleDirectory(arg string) {
+	// Read all files in directory
+	files, err := ioutil.ReadDir(arg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load data of all png we find
+	for _, file := range files {
+		if !file.Mode().IsDir() {
+			filePath := path.Join(arg, file.Name())
+			handleIndividualFile(filePath)
+		}
+	}
+}
+
+func handleIndividualFile(arg string) {
+	// Check file type
+	if f, _ := isPNG(arg); f == true { // Handle PNG
+
+		// Try to open file
+		imgFile, err := os.Open(arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer imgFile.Close()
+
+		// Get image object data
+		imgData, _, err := image.Decode(imgFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Load additional image data and store in array
+		imgEntry, err := getImageData(&imgData, arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		images = append(images, &imgEntry)
+
+		fileCount++
+		log.Printf("Loaded image [%d]: %s: %dx%d", fileCount, imgEntry.path, imgEntry.width, imgEntry.height)
+
+	} else { // Handle GIF
+		// File not supported
+	}
+
 }
 
 func usage() {
@@ -168,90 +219,25 @@ func main() {
 	flag.Usage = usage
 	flag.Parse()
 
-	images := []*ImageData{}
-	fileCount := 0
-
 	// Bail out if no additional arguments
 	if len(flag.Args()) == 0 {
 		log.Fatal("No files or folder provided")
 	}
 
-	// TODO: Put in own function at some point
 	// Go through arguments and load image data
 	for _, arg := range flag.Args() {
 
+		// Get file type (file or dir)
 		fi, err := os.Stat(arg)
 		if err != nil {
-			log.Fatal(err)	
+			log.Fatal(err)
 		}
 
-		if fi.Mode().IsDir() { // Handle directory
-
-			// Read all files in directory
-		    files, _ := ioutil.ReadDir(arg)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Load data of all png we find
-		    for _, file := range(files) {
-		    	if !file.Mode().IsDir() {
-		    		filePath := path.Join(arg, file.Name())
-		    		if f, _ := isPNG(filePath); f == true {
-
-						// Try to open file
-						imgFile, err := os.Open(filePath)
-						if err != nil {
-							log.Fatal(err)
-						}
-						defer imgFile.Close()
-					
-						// Get image object data 
-						imgData, _, err := image.Decode(imgFile)
-						if err != nil {
-							log.Fatal(err)
-						}
-				
-						// Load additional image data and store in array
-						imgEntry, err := getImageData(&imgData, filePath)
-						if err != nil {
-							log.Fatal(err)
-						}
-						images = append(images, &imgEntry)
-		    			log.Printf("Loaded image [%d]: %s: %dx%d", fileCount+1, imgEntry.path, imgEntry.width, imgEntry.height)
-						fileCount++
-		    		}
-		    	}
-			}
-		} else { // Handle individual files
-
-			// Check file type
-			if f, _ := isPNG(arg); f == true {
-
-				// Try to open file
-				imgFile, err := os.Open(arg)
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer imgFile.Close()
-					
-				// Get image object data 
-				imgData, _, err := image.Decode(imgFile)
-				if err != nil {
-					log.Fatal(err)
-				}
-				
-				// Load additional image data and store in array
-				imgEntry, err := getImageData(&imgData, arg)
-				if err != nil {
-					log.Fatal(err)
-				}
-				images = append(images, &imgEntry)
-
-				log.Printf("Loaded image [%d]: %s: %dx%d", fileCount+1, imgEntry.path, imgEntry.width, imgEntry.height)
-				fileCount++
-				
-			}	
+		// Work out if path in argument points to a directory or a file
+		if fi.Mode().IsDir() {
+			handleDirectory(arg)
+		} else {
+			handleIndividualFile(arg)
 		}
 	}
 
